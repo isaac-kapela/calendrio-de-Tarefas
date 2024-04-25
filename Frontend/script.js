d0ce046db3aa2058c69aada1b05fda8c288ed2df
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2024-04-25 17:48:28 -0300

    criando a função addOuvinte

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,18 +1,18 @@
 const calendario = document.querySelector('calendario'),
-data = document.querySelector(".data"),
-diasContainer = document.querySelector(".dias"),
-anterior = document.querySelector(".anterior"),
-proximo = document.querySelector(".proximo"),
-hojeBotao = document.querySelector(".hoje-btn"),
-reuniaoBotao = document.querySelector(".reuniao-btn"),
-dataInput = document.querySelector(".data-input");
+    data = document.querySelector(".data"),
+    diasContainer = document.querySelector(".dias"),
+    anterior = document.querySelector(".anterior"),
+    proximo = document.querySelector(".proximo"),
+    hojeBotao = document.querySelector(".hoje-btn"),
+    reuniaoBotao = document.querySelector(".reuniao-btn"),
+    dataInput = document.querySelector(".data-input");
 
 let hoje = new Date();
 let ativoDia;
 let mes = hoje.getMonth();
 let ano = hoje.getFullYear();
 
-const meses = [ "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro" ];
+const meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
 
 //Array de eventos padrão
 
@@ -25,14 +25,14 @@ let eventosLista = [
             nome: "Reunião",
             horario: "10:00",
             descricao: "Reunião com a equipe de marketing"
-        
+
         },
-    ],
+        ],
     },
 ];
 
 // funçao para adicionar dias
-function iniciarCalendario(){
+function iniciarCalendario() {
     //para obter os dias do mês anterior e o mês atual, todos os dias e os dias restantes do próximo mês
     const primeiroDia = new Date(ano, mes, 1);
     const ultimoDia = new Date(ano, mes + 1, 0);
@@ -40,7 +40,7 @@ function iniciarCalendario(){
     const anteriorDia = anteriorUltimoDia.getDate();
     const ultimoDiaMes = ultimoDia.getDate();
     const dia = primeiroDia.getDay();
-    const proximoDias = 7 - ultimoDia.getDay() - 1; 
+    const proximoDias = 7 - ultimoDia.getDay() - 1;
 
     //atualizar data no topo do calendário
     data.innerHTML = meses[mes] + " " + ano;
@@ -49,54 +49,56 @@ function iniciarCalendario(){
     let dias = "";
 
     //meses anteriores dias
-    for(let x = dia; x > 0; x--){
+    for (let x = dia; x > 0; x--) {
         dias += `<div class="dia anterior">${ultimoDiaMes - x + 1}</div>`;
     }
-     //dias do mês atual
-    for(let i = 1; i <= ultimoDiaMes; i++){ 
+    //dias do mês atual
+    for (let i = 1; i <= ultimoDiaMes; i++) {
 
         let temEvento = false;
         eventosLista.forEach((eventoObj) => {
-            if(eventoObj.dia === i && eventoObj.mes === mes + 1 && eventoObj.ano === ano){
+            if (eventoObj.dia === i && eventoObj.mes === mes + 1 && eventoObj.ano === ano) {
                 temEvento = true;
             }
-        
+
         })
 
         //se o dia for hoje adicione a aula hoje
-        if(i === new Date().getDate()&& ano === new Date().getFullYear() && mes === new Date().getMonth()) {
-           if(temEvento){
-            dias += `<div class="dia hoje evento">${i}</div>`;
-           }
-           else{
-            dias += `<div class="dia hoje">${i}</div>`;
-           }
+        if (i === new Date().getDate() && ano === new Date().getFullYear() && mes === new Date().getMonth()) {
+            if (temEvento) {
+                dias += `<div class="dia hoje evento">${i}</div>`;
+            }
+            else {
+                dias += `<div class="dia hoje">${i}</div>`;
+            }
         }
         //adiciona o restante como está
-        else{
-            if(temEvento){
+        else {
+            if (temEvento) {
                 dias += `<div class="dia  evento">${i}</div>`;
-               }
-               else{
+            }
+            else {
                 dias += `<div class="dia ">${i}</div>`;
-               }
+            }
         }
     }
 
     //dias do próximo mês
-    for(let j = 1; j <= proximoDias; j++){
+    for (let j = 1; j <= proximoDias; j++) {
         dias += `<div class="dia proximo">${j}</div>`;
     }
     diasContainer.innerHTML = dias;
+
+    addOuvinte();
 }
 
 iniciarCalendario();
 
 //mês anterior
 
-function mesAnterior(){
+function mesAnterior() {
     mes--;
-    if(mes < 0){
+    if (mes < 0) {
         mes = 11;
         ano--;
     }
@@ -104,9 +106,9 @@ function mesAnterior(){
 }
 
 //próximo mês
-function proximoMes(){
+function proximoMes() {
     mes++;
-    if(mes > 11){
+    if (mes > 11) {
         mes = 0;
         ano++;
     }
@@ -127,29 +129,29 @@ hojeBotao.addEventListener("click", () => {
 dataInput.addEventListener("chave", (e) => {
     //permitir apenas números remover qualquer outra coisa
     dataInput.value = dataInput.value.replace(/[^0-9]/g, "");
-    if(dataInput.value.length === 2){
+    if (dataInput.value.length === 2) {
         dataInput.value += "/";
     }
 
-    if(dataInput.value.length > 7){
+    if (dataInput.value.length > 7) {
         dataInput.value = dataInput.value.slice(0, 7);
     }
-    if(e.inputType = "excluirConteúdoParaTras"){
-        if(dataInput.value.length === 3){
-        dataInput.value = dataInput.value.slice(0, 2);
+    if (e.inputType = "excluirConteúdoParaTras") {
+        if (dataInput.value.length === 3) {
+            dataInput.value = dataInput.value.slice(0, 2);
         }
     }
 });
 
-reuniaoBotao.addEventListener("click",  reuniaoData);
+reuniaoBotao.addEventListener("click", reuniaoData);
 
 //função para ir para a data inserida
 
-function reuniaoData(){
+function reuniaoData() {
     const dataDeChegada = dataInput.value.split("/");
-    if(dataDeChegada.length === 2){
-        if(dataDeChegada[0] > 0 && dataDeChegada[0] < 13 && dataDeChegada[1].length ===4){
-            mes === dataDeChegada[0] -1;
+    if (dataDeChegada.length === 2) {
+        if (dataDeChegada[0] > 0 && dataDeChegada[0] < 13 && dataDeChegada[1].length === 4) {
+            mes === dataDeChegada[0] - 1;
             ano = dataDeChegada[1];
             iniciarCalendario();
             return;
@@ -159,11 +161,11 @@ function reuniaoData(){
 }
 
 const addEventoBtn = document.querySelector(".add-evento"),
- addEventoConteiner = document.querySelector(".add-evento-box"),
-  addEventoFecharBtn = document.querySelector(".close"),
-  addEventoTitulo = document.querySelector(".evento-nome"),
-  addEventoForms = document.querySelector(".horario-evento"),
-  addEventoDescricao = document.querySelector(".descricao-evento");
+    addEventoConteiner = document.querySelector(".add-evento-box"),
+    addEventoFecharBtn = document.querySelector(".close"),
+    addEventoTitulo = document.querySelector(".evento-nome"),
+    addEventoForms = document.querySelector(".horario-evento"),
+    addEventoDescricao = document.querySelector(".descricao-evento");
 
 
 addEventoBtn.addEventListener("click", () => {
@@ -174,8 +176,8 @@ addEventoFecharBtn.addEventListener("click", () => {
 });
 
 
-document.addEventListener("click", (e) => { 
-    if(e.target !== addEventoBtn  &&  !addEventoConteiner.contains(e.target)){
+document.addEventListener("click", (e) => {
+    if (e.target !== addEventoBtn && !addEventoConteiner.contains(e.target)) {
         addEventoConteiner.classList.remove("ativo");
     }
 });
@@ -189,10 +191,10 @@ addEventoTitulo.addEventListener("input", (e) => {
 addEventoForms.addEventListener("input", (e) => {
 
     addEventoForms.value = addEventoForms.value.replace(/[^0-9:]/g, "");
-    if(addEventoForms.value.length === 2){
+    if (addEventoForms.value.length === 2) {
         addEventoForms.value += ":";
     }
-    if(addEventoForms.value.length > 5){
+    if (addEventoForms.value.length > 5) {
         addEventoForms.value = addEventoForms.value.slice(0, 5);
 
     }
@@ -207,11 +209,39 @@ addEventoTitulo.addEventListener("input", (e) => {
 addEventoDescricao.addEventListener("input", (e) => {
 
     addEventoDescricao.value = addEventoDescricao.value.replace(/[^0-9:]/g, "");
-    if(addEventoDescricao.value.length === 2){
+    if (addEventoDescricao.value.length === 2) {
         addEventoDescricao.value += ":";
     }
-    if(addEventoDescricao.value.length > 5){
+    if (addEventoDescricao.value.length > 5) {
         addEventoDescricao.value = addEventoDescricao.value.slice(0, 5);
 
     }
 });
+
+function addOuvinte() {
+    const dias = document.querySelectorAll(".dia");
+    dias.forEach((dia) => {
+        dia.addEventListener("click", (e) => {
+            ativoDia = Number(e.target.innerHTML);
+
+            dias.forEach((dia) => {
+                dia.classList.remove("ativo");
+            });
+
+            if (e.target.classList.contains("anterior")) {
+                mesAnterior();
+
+                setTimeout(() => {
+                    const dias = document.querySelectorAll(".dia");
+
+                    dias.forEach((dia) => {
+                        if (!dia.classList.contains("anterior") && dia.innerHTML === e.target.innerHTML) {
+                            dia.classList.add("ativo");
+                        }
+                    });
+                }, 100);
+            }
+        });
+
+    });
+}
